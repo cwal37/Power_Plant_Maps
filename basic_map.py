d22c1db5c49398b90f7a817948b79e69057263a2
cwal37 <cwal37@gmail.com>
2016-11-16 23:16:43 -0500

    Beginning of housekeeping

--- basic_map.py
+++ basic_map.py
@@ -6,4 +6,10 @@ Created on Wed Nov 16 22:54:50 2016
 """
 
 import pandas as pd
-import ploty
+#import ploty
+import pdb
+
+
+df = pd.read_csv('epa_data\\dfiles.csv', index_col=False)
+print df.columns
+#pdb.set_trace()
